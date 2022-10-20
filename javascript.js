64103f5c84e28fb690925d76879ba62d3fc2df0f
Kyle Hilbert <104146726+KyleHilbert@users.noreply.github.com>
2022-10-20 11:04:12 -0400

    Changed returns for functions

--- javascript.js
+++ javascript.js
@@ -3,16 +3,16 @@ function addition(num1, num2) {
 }
 
 function subtraction(num1, num2) {
-  return num1 - num2;
+  return parseInt(num1) - parseInt(num2);
 }
 
 function multiplication(num1, num2) {
-  return num1 * num2;
+  return parseInt(num1) * parseInt(num2);
 }
 
 function division(num1, num2) {
   if (num2 > 0) {
-    return num1 / num2;
+    return parseInt(num1) / parseInt(num2);
   } else {
     return "Cannot divide by 0";
   }
